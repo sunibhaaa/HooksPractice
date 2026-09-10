0aa413fd486cd8ff9b58ca649662ca83fb145a2f
Sunibha Maskey <sunibhamaskey1@gmail.com>
2026-09-10 13:42:34 +0545

    useState with input text.

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+  const [name, setName] = useState("");
+
+  function increaseCounter() {
+    setCounter(counter + 1)
+  }
+
+  return (
+    <div>
+      <input type="text" onChange={e => setName(e.target.value)} />
+      <h1>{name} has been clicked {counter} times!</h1>
+      <button onClick={increaseCounter}>Increase</button>
+    </div>
+  );
+}
+
+export default App; 
