2fd78bb8be5898c3445bd099b4f0f41f06a6184e
Sunibha Maskey <sunibhamaskey1@gmail.com>
2026-09-10 14:13:02 +0545

    useEffect clean up function: TIMER

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from "react";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [otherCount, setOtherCount] = useState(5);
+  const [time, setTime] = useState(0);
 
   useEffect(() => {
-    document.title = `${otherCount} new Messages!`
-  }, [otherCount]);
+    const timer = setInterval(() => {
+      setTime(time + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    }
+  });
+
   return (
     <div>
-      <h3>{count} new Messages!</h3>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-      <h3>Other Count: {otherCount}</h3>
-      <button onClick={() => setOtherCount(otherCount + 5)}>Increase by 5</button>
+      <h3>{time} in seconds</h3>
     </div>
   );
 }
-
-export default App; 
+export default App;
