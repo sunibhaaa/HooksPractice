e8087f5479fc6a037be260e75722dce277c0a72b
Sunibha Maskey <sunibhamaskey1@gmail.com>
2026-09-10 14:06:50 +0545

    useEffect with variables.

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [otherCount, setOtherCount] = useState(5);
 
   useEffect(() => {
-    document.title = `${count} new Messages!`
-  });
+    document.title = `${otherCount} new Messages!`
+  }, [otherCount]);
   return (
     <div>
       <h3>{count} new Messages!</h3>
       <button onClick={() => setCount(count + 1)}>Increase</button>
+      <h3>Other Count: {otherCount}</h3>
+      <button onClick={() => setOtherCount(otherCount + 5)}>Increase by 5</button>
     </div>
   );
 }
